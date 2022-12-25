62aa3cab6e20a1f5fa8070a2ecc654c156dddf30
Ankush Ukey <114287684+ankushx@users.noreply.github.com>
2022-12-25 12:25:59 +0530

    Update subset.cpp

--- recursion/subset.cpp
+++ recursion/subset.cpp
@@ -1,3 +1,30 @@
+//method 1
+class Solution {
+public:
+    void find_subset(vector<int> nums,vector<int> output,int idx,vector<vector<int>>& ans)
+    {
+       
+            ans.push_back(output);
+           
+          
+       for(int i=idx;i<nums.size();i++)
+       {
+           output.push_back(nums[i]);
+           find_subset(nums,output,i+1,ans);
+           output.pop_back();
+       }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int idx=0;
+        find_subset(nums,output,idx,ans);
+        return ans;
+    }
+};
+
+
+//method 2
 class Solution {
 public:
     void find_subset(vector<int> nums,vector<int> output,int idx,vector<vector<int>>& ans)
