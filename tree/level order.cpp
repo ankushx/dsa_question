e048327f550a1bf363aac5bfe25922f7556d24e5
Ankush Ukey <114287684+ankushx@users.noreply.github.com>
2023-02-09 13:56:07 +0530

    Create level order.cpp

---
+++ tree/level order.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+         vector<vector<int>> ans;
+    if(root ==NULL)
+        return ans;
+        queue<TreeNode*> st;
+        st.push(root);
+       
+        while(!st.empty())
+        {
+            int n=st.size();
+            vector<int> level;
+            for(int i=0;i<n;i++)
+            {
+                TreeNode* curr=st.front();
+                st.pop();
+                level.push_back(curr->val);
+                if(curr->left)
+                    st.push(curr->left);
+                if(curr->right)
+                    st.push(curr->right);
+               // cout<<curr->val<<" - ";
+            }
+          //  cout<<endl;
+            ans.push_back(level);
+           
+        }
+        return ans;
+    }
+};
+
+
